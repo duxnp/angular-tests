83f4609b8ce1dcabcf934d5b0010d8ea291ffcf3
badboybry9000 <badboybry9000@gmail.com>
2022-02-18 16:09:50 -0500

    refactor: switch from moment to luxon

--- apps/beluvian-calendar/src/app/app.component.ts
+++ apps/beluvian-calendar/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
+import { DateTime } from 'luxon';
 
-import * as moment from 'moment';
 import { Week } from './models/week';
 import { Day } from './models/day';
 import { DayNames } from './models/day-names';
@@ -11,110 +11,104 @@ import { DayNames } from './models/day-names';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  now: moment.Moment;
-  today: moment.Moment;
-  year: number;
+  year: number = DateTime.now().year;
   weeks: Week[] = [];
-  days: Day[] = [];
 
   constructor() {
-    this.now = moment();
-    this.today = moment();
-    this.year = this.now.year();
-
-    this.momentTest1();
-    this.generateCalendar();
+    this.weeks = this.generateCalendar(this.year);
   }
 
   previousYear() {
     this.year--;
-    this.generateCalendar();
+    this.weeks = this.generateCalendar(this.year);
   }
 
   nextYear() {
     this.year++;
-    this.generateCalendar();
+    this.weeks = this.generateCalendar(this.year);
   }
 
-  momentTest2() {
+  dateLibTest1() {
+    const nowLuxon = DateTime.now();
+
+    console.log(nowLuxon.weeksInWeekYear); // equivalent of isoWeeksInYear();
+    console.log(nowLuxon.day); // Day part of moment date
+    console.log(nowLuxon.weekday); // Day of week
+    console.log(nowLuxon.ordinal); // Day of year
+    console.log(nowLuxon.daysInYear); // Day of year
+    console.log(nowLuxon.weekNumber); // ISO Week of year
+    console.log(nowLuxon.daysInMonth); // Days in month
+    console.log(nowLuxon.isInLeapYear); // Is it a leap year
+    console.log(nowLuxon.year); // Year
+    console.log(nowLuxon.toLocaleString());
+    console.log(nowLuxon.startOf('year').toLocaleString());
+    console.log(nowLuxon.endOf('year').toLocaleString());
+  }
+
+  dateLibTest2() {
     const year = 2000;
-    console.log(moment([year]).isLeapYear());
-    console.log(moment([year]).weeksInYear());
-    const a = moment([year]).startOf('year');
-    const b = moment([year]).endOf('year');
+    const luxonYear = DateTime.fromObject({ year });
+    console.log(luxonYear.isInLeapYear);
+    console.log(luxonYear.weeksInWeekYear);
 
-    console.log(a.diff(b, 'days'));
+    const aLuxon = luxonYear.startOf('year');
+    const bLuxon = luxonYear.endOf('year');
+    console.log(Math.floor(bLuxon.diff(aLuxon, 'days').days));
 
-    console.log(moment('2000-2-29', 'YYYY-MM-DD').dayOfYear());
-  }
-
-  momentTest1() {
-    console.log(this.now.weeksInYear()); // Weeks in year
-    console.log(this.now.date()); // Day part of moment date
-    console.log(this.now.day()); // Day of week
-    console.log(this.now.dayOfYear()); // Day of year
-    console.log(this.now.week()); // Week of year
-    console.log(this.now.daysInMonth()); // Days in month
-    console.log(this.now.isLeapYear()); // Is it a leap year
-    console.log(this.now.year()); // Year
-
-    console.log();
-
-    // console.log(moment().startOf('year').format('MM/DD/YYYY'));
-    console.log(this.now.format('MM/DD/YYYY'));
-    console.log(this.now.startOf('year').format('MM/DD/YYYY'));
-    console.log(this.now.endOf('year').format('MM/DD/YYYY'));
-
-    console.log(this.now.dayOfYear()); // Day of year
+    console.log(DateTime.fromFormat('2000-02-29', 'y-L-d').ordinal);
   }
 
   isToday(day: Day) {
     return (
       day.dayOfYear !== undefined &&
-      day.dayOfYear === moment().dayOfYear() &&
-      day.year === moment().year()
+      day.dayOfYear === DateTime.now().ordinal &&
+      day.year === DateTime.now().year
     );
   }
 
-  generateCalendar() {
-    const calendar = moment([this.year]).startOf('year');
-    const startOf = moment([this.year]).startOf('year');
-    const isLeapYear = calendar.isLeapYear();
-    const weeksInYear = calendar.weeksInYear();
-    const daysInYear = isLeapYear ? 366 : 365;
+  generateCalendar(year: number): Week[] {
+    // Go to the first day of the requested year
+    // This will be reassigned as the loop goes through each day of the year
+    let calendar = DateTime.fromObject({ year }).startOf('year');
+    const weeksInYear = calendar.weeksInWeekYear;
     const dayNames = new DayNames();
-
-    this.weeks = [];
-    this.days = [];
+    const weeks: Week[] = [];
 
     // Each week of the year
     for (let i = 0; i <= weeksInYear; i++) {
-      this.weeks.push(new Week(i + 1, []));
+      weeks.push(new Week(i + 1, []));
 
       // Each day of each week
       for (let j = 0; j < 7; j++) {
-        const daysBetween = calendar.diff(startOf, 'days');
+        const daysBetween = calendar.diff(calendar.startOf('year'), 'days');
+
+        // Moment weekday numbers go from 0 to 6 with 0 being Sunday
+        // Luxon weekday numbers go from 1 to 7 with 7 being Sunday
+        // Moment: 0 1 2 3 4 5 6
+        // Luxon:  7 1 2 3 4 5 6
+        const weekday = calendar.weekday === 7 ? 0 : calendar.weekday;
 
         // Starting at Sunday, add empty calendar days if not at the first day of the year yet.
         // Also add empty calendar days if past the last day of the year
-        if (j < calendar.day() || daysBetween >= daysInYear) {
-          // was new Day(j + 1, undefined, undefined, undefined)
-          this.weeks[i].days.push(new Day(j + 1, 0, 0, ''));
+        if (j < weekday || daysBetween.days >= calendar.daysInYear) {
+          weeks[i].days.push(new Day(j + 1, 0, 0, ''));
         } else {
-          this.weeks[i].days.push(
+          weeks[i].days.push(
             new Day(
               j + 1,
-              calendar.dayOfYear(),
-              calendar.year(),
-              dayNames.getName(calendar.dayOfYear(), isLeapYear) + 'day'
+              calendar.ordinal,
+              calendar.year,
+              dayNames.getName(calendar.ordinal, calendar.isInLeapYear) + 'day'
             )
           );
-          calendar.add(1, 'days');
+          calendar = calendar.plus({ days: 1 });
         }
       }
     }
 
     // console.dir(this.weeks);
     // console.log(this.weeks[1].days[2].dayName);
+
+    return weeks;
   }
 }
