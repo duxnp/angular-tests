dd8eb9e5fe86c7fdb37ea341e73884087c7e4487
badboybry9000 <badboybry9000@gmail.com>
2022-03-07 11:10:51 -0500

    test: add test utils library

--- libs/b-cal/year/ui/src/lib/day-card/day-card.component.spec.ts
+++ libs/b-cal/year/ui/src/lib/day-card/day-card.component.spec.ts
@@ -1,5 +1,5 @@
+import { mockPipe } from '@angular-tests/shared/test-utils';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DayCardComponent } from './day-card.component';
 
 describe('DayCardComponent', () => {
@@ -8,13 +8,21 @@ describe('DayCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DayCardComponent],
+      declarations: [DayCardComponent, mockPipe({ name: 'day' }, 'foo')],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DayCardComponent);
     component = fixture.componentInstance;
+    const mockDay = {
+      dayOfWeek: 1,
+      dayOfYear: 1,
+      isWeekend: false,
+      beday: { id: 1, abbreviation: 'foo', name: 'foo' },
+    };
+    component.day = mockDay;
+    component.today = mockDay;
     fixture.detectChanges();
   });
 
--- libs/shared/entity-store/src/lib/services/entity-pagination.service.spec.ts
+++ libs/shared/entity-store/src/lib/services/entity-pagination.service.spec.ts
@@ -3,7 +3,8 @@ import { TestBed } from '@angular/core/testing';
 import { EntityPaginationService } from './entity-pagination.service';
 
 describe('EntityPaginationService', () => {
-  let service: EntityPaginationService;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let service: EntityPaginationService<any>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
