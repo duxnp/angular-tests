44ac2044d9ac44d81fb4a03099ce73b5947f7b63
Bryan Kowal <badboybry9000@gmail.com>
2022-03-30 01:22:54 -0400

    feat: improve nav

--- libs/b-cal/year/feature/src/lib/containers/year/year.component.ts
+++ libs/b-cal/year/feature/src/lib/containers/year/year.component.ts
@@ -3,16 +3,19 @@ import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { Router, RouterModule } from '@angular/router';
 import { ReactiveComponentModule } from '@ngrx/component';
 import { Store } from '@ngrx/store';
+import { DateTime } from 'luxon';
 import { combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { YearsSelectors } from '@ng-tests/b-cal/year/data-access';
 import { CalendarModule, DayCardModule } from '@ng-tests/b-cal/year/ui';
 import { Day } from '@ng-tests/b-cal/year/util';
-import { filterNullish } from '@ng-tests/shared/util';
+import { filterNullish, LuxonLimits } from '@ng-tests/shared/util';
 
 @Component({
   selector: 'bc-year',
@@ -39,6 +42,18 @@ export class YearComponent {
     this.router.navigate([year]);
   }
 
+  firstYear() {
+    this.router.navigate([LuxonLimits.YEAR_MIN]);
+  }
+
+  lastYear() {
+    this.router.navigate([LuxonLimits.YEAR_MAX]);
+  }
+
+  currentYear() {
+    this.router.navigate([DateTime.now().year]);
+  }
+
   onDayClick(day: Day) {
     this.router.navigate([day.year, day.beday?.id]);
   }
@@ -52,6 +67,8 @@ export class YearComponent {
     FlexLayoutModule,
     MatButtonModule,
     MatIconModule,
+    MatMenuModule,
+    MatTooltipModule,
     DayCardModule,
     CalendarModule,
   ],
