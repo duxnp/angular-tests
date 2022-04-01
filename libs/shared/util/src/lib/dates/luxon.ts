cebf206c70475ed86b413ce860be7c33af3cf566
badboybry9000 <badboybry9000@gmail.com>
2022-04-01 17:22:17 -0400

    fix: adjust luxon limits

--- libs/shared/util/src/lib/dates/luxon.ts
+++ libs/shared/util/src/lib/dates/luxon.ts
@@ -1,8 +1,12 @@
 import { DateTime } from 'luxon';
 
+/**
+ * The range of year values luxon supports.
+ * It starts at -271820 and stops working halfway through 275760
+ */
 export const LuxonLimits = {
   YEAR_MIN: -271820,
-  YEAR_MAX: 275760,
+  YEAR_MAX: 275759,
 } as const;
 
 /**
@@ -10,7 +14,7 @@ export const LuxonLimits = {
  * Luxon weekday numbers go from 1 to 7 with 7 being Sunday.
  *
  * - Moment: 0 1 2 3 4 5 6
- * - Luxon:  7 1 2 3 4 5 6
+ * -  Luxon: 7 1 2 3 4 5 6
  *
  * @param calendar
  * @returns
