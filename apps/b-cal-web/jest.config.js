a15994f66d4d004c7cbb2afa76cdea0c84238edc
badboybry9000 <badboybry9000@gmail.com>
2022-04-18 15:22:59 -0400

    refactor: rename b-cal app project

--- apps/b-cal-web/jest.config.js
+++ apps/b-cal-web/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'b-cal',
+  displayName: 'b-cal-web',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/b-cal',
+  coverageDirectory: '../../coverage/apps/b-cal-web',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
