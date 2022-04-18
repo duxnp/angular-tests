77d85b346aba63285c87a6263cfffa8a45813354
badboybry9000 <badboybry9000@gmail.com>
2022-04-18 14:42:57 -0400

    test: make tests pass

--- libs/tailwind/workcation/feature/src/lib/containers/city-card/city-card.component.spec.ts
+++ libs/tailwind/workcation/feature/src/lib/containers/city-card/city-card.component.spec.ts
@@ -1,21 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Destination } from '../models/Destination';
 import { CityCardComponent } from './city-card.component';
 
 describe('CityCardComponent', () => {
   let component: CityCardComponent;
   let fixture: ComponentFixture<CityCardComponent>;
+  let destination: Destination;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CityCardComponent ]
-    })
-    .compileComponents();
+      declarations: [CityCardComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CityCardComponent);
     component = fixture.componentInstance;
+
+    destination = {
+      city: 'Toronto',
+      averagePrice: 120,
+      propertyCount: 76,
+      imageUrl: 'https://source.unsplash.com/qlKaN7eqay8/600x600',
+      imageAlt: 'Toronto skyline',
+    };
+    component.destination = destination;
     fixture.detectChanges();
   });
 
--- libs/tailwind/workcation/feature/src/lib/containers/property-card/property-card.component.spec.ts
+++ libs/tailwind/workcation/feature/src/lib/containers/property-card/property-card.component.spec.ts
@@ -1,21 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  waitForAsync
+} from '@angular/core/testing';
 
+import { Property } from '../models/Property';
 import { PropertyCardComponent } from './property-card.component';
 
 describe('PropertyComponent', () => {
   let component: PropertyCardComponent;
   let fixture: ComponentFixture<PropertyCardComponent>;
+  let property: Property;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PropertyCardComponent ]
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [PropertyCardComponent],
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PropertyCardComponent);
     component = fixture.componentInstance;
+    property = {
+      imageUrl: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750',
+      imageAlt: 'Rear view of modern home with pool',
+      beds: 3,
+      baths: 2,
+      title: 'Modern executive home in the heart of historic Los Angeles',
+      priceInCents: 190000,
+      formattedPrice: '$1,900.00',
+      reviewCount: 34,
+      rating: 4,
+    };
+    component.property = property;
     fixture.detectChanges();
   });
 
