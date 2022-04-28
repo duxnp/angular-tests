c51a2b444242084edc6528c0999d80847656dc35
Bryan Kowal <badboybry9000@gmail.com>
2022-04-28 15:31:04 -0400

    feat: add color scheme

--- apps/b-cal-mobile-e2e/src/pages/app.po.ts
+++ apps/b-cal-mobile-e2e/src/pages/app.po.ts
@@ -2,7 +2,4 @@ export class App {
   public visit(): void {
     cy.visit('/');
   }
-
-  // TODO: find out how to select the popover
-  // public matDialog = () => cy.get('mat-dialog-container');
 }
